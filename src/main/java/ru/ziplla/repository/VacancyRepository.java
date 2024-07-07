a0a1efe8b08941ad9233e3d71473a7e5f6debffc
Pavel <pzajtsev@yandex.ru>
2024-07-07 04:18:33 +0400

    edit VacancyRepository

--- src/main/java/ru/ziplla/repository/VacancyRepository.java
+++ src/main/java/ru/ziplla/repository/VacancyRepository.java
@@ -1,7 +1,9 @@
 package ru.ziplla.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.ziplla.entity.Vacancy;
 
+@Repository
 public interface VacancyRepository extends JpaRepository<Vacancy, Integer> {
 }
